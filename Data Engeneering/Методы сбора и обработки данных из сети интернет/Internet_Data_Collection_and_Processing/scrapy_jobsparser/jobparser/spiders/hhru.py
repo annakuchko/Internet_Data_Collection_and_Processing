1052c82bfec4dd3b886b613129e25c41706cdc71
annakuchko <anna.kuchko@yandex.ru>
2021-03-21 23:17:40 +0400

    job parser commit

---
+++ Data Engeneering/Методы сбора и обработки данных из сети интернет/Internet_Data_Collection_and_Processing/scrapy_jobsparser/jobparser/spiders/hhru.py
@@ -0,0 +1,55 @@
+import scrapy
+from scrapy.http import HtmlResponse
+from jobparser.items import JobparserItem
+# *Наименование вакансии
+# *Зарплата от
+# *Зарплата до
+# *Ссылку на саму вакансию
+
+# *Сайт откуда собрана вакансия
+# И складывал все записи в БД(любую)
+
+
+class HhruSpider(scrapy.Spider):
+    name = 'hhru'
+    allowed_domains = ['hh.ru']
+    start_urls = ['https://samara.hh.ru/vacancies/analyst']
+
+    def start_requests(self):
+        urls = self.start_urls
+        
+        for url in urls:
+            req= scrapy.Request(url=url, callback=self.parse)
+            req.headers['Cookie'] = 'js_enabled=true; is_cookie_active=true;'
+            yield req
+            
+    def parse(self, response: HtmlResponse):
+        print(response.url)
+        vacancy_links = response.xpath(
+            '//*[contains(@class, "vacancy-serp-item")]//*[contains(@data-qa,'
+            ' "title")]/@href'
+            ).extract()
+        for link in vacancy_links:
+            print(link)
+            yield response.follow(link, callback=self.parse_vacancies)
+            
+        next_page = response.xpath(
+            '//*[contains(@data-qa, "pager-next")]/@href'
+            ).get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+        
+        pass
+    
+    def parse_vacancies(self, response: HtmlResponse):
+        title = response.xpath(
+            "//*[contains(@class, 'vacancy-title')]//h1//text()"
+            ).extract()
+        salary = response.xpath(
+            "//*[contains(@class, 'vacancy-salary')]/span/text()"
+            ).extract()
+        url = response.url
+        # print('')
+        yield JobparserItem(title = title, salary = salary, url = url)
+        pass
+        
