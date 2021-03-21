fdbe1d5b921e8c01d71d10404cc583393dcff9f2
annakuchko <66664241+annakuchko@users.noreply.github.com>
2021-03-22 02:19:58 +0400

    Add files via upload

---
+++ scrapy_bookparser/bookparser/items.py
@@ -0,0 +1,19 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BookparserItem(scrapy.Item):
+    
+    _id = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
+    authors = scrapy.Field()
+    new_price = scrapy.Field()
+    old_price = scrapy.Field()
+    book_rating = scrapy.Field()
+    
+    pass
