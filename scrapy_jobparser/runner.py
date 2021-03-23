205b007cdf0023991b6520a76036f67261af3751
annakuchko <66664241+annakuchko@users.noreply.github.com>
2021-03-23 23:07:36 +0400

    Update runner.py

--- scrapy_jobparser/runner.py
+++ scrapy_jobparser/runner.py
@@ -4,12 +4,11 @@ from jobparser.spiders.hhru import HhruSpider
 from jobparser.spiders.sjru import SjruSpider
 from jobparser import settings
 
-
 if __name__ == "__main__":
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
     
-    process = CrawlerProcess(settings = crawler_settings)
+    process = CrawlerProcess(settings=crawler_settings)
     process.crawl(HhruSpider)
     process.crawl(SjruSpider)
     
